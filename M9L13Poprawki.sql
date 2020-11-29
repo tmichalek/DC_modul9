301fd87fe214459f5067b856d4066bafd3a1dd34
Tomasz Michałek <2355@student.ujw.pl>
2020-11-29 12:00:22 +0100

    Poprawki 2 modul 9

--- M9L13Poprawki.sql
+++ M9L13Poprawki.sql
@@ -27,14 +27,17 @@ create or replace function expense_tracker.budget()
    $$
         begin
             --RAISE NOTICE 'Co jest w TG_OP: %', TG_OP;
-                if (tg_op ='DELETE') then
-                    update expense_tracker.monthly_budget_planned set left_budget = (left_budget - old.transaction_value );
+                if (tg_op ='DELETE' and (old.id_trans_type=2 or old.id_trans_type=3)) then 
+                    update expense_tracker.monthly_budget_planned set left_budget = (left_budget - old.transaction_value )
+                    where expense_tracker.monthly_budget_planned.year_month = concat((extract (year from now()))::text, '_' , (extract (month from now()))::text);
                         
-                elseif (tg_op = 'UPDATE') then
-                    update expense_tracker.monthly_budget_planned set left_budget = (left_budget + new.transaction_value );
+                elseif (tg_op ='UPDATE' and (new.id_trans_type=2 or new.id_trans_type=3)) then
+                    update expense_tracker.monthly_budget_planned set left_budget = (left_budget - (old.transaction_value - new.transaction_value))
+                where expense_tracker.monthly_budget_planned.year_month = concat(extract (year from now())::text, '_' , (extract (month from now()))::text);
                     
-                elseif (tg_op ='INSERT') then
-                   update expense_tracker.monthly_budget_planned set left_budget = (left_budget + new.transaction_value );
+                elseif (tg_op ='INSERT' and (new.id_trans_type=2 or new.id_trans_type=3)) then
+                   update expense_tracker.monthly_budget_planned set left_budget = (left_budget + new.transaction_value )
+               where expense_tracker.monthly_budget_planned.year_month =concat(extract (year from now())::text,  '_' , (extract (month from now()))::text);
                
                 end if; 
     return null;
@@ -46,8 +49,13 @@ after insert or update or delete
 on expense_tracker.transactions 
 for each row
 execute procedure expense_tracker.budget(); 
-                      
- --Trigger nie uwzględnia 
- -- a) dodania kolejnego miesiąca transakcji (chyba że kontrolujemy to inną procedurą), 
---  b) w przypadku usuwania jakiejsć transakcji jesli był to wydatek należało by kwotę tą 
---     dodać do planowanego budżetu, a w przypadku dochodu odjąć 
+
+-- w trigerze przyjałem format danych w wierszu year_month jako np 11_2020, 07_2020                    
+ --Trigger nie uwzglÃ„â„¢dnia 
+ -- a) dodania kolejnego miesiÃ„â€¦ca transakcji w przypadku zmiany daty (chyba Ã…Â¼e kontrolujemy to innÃ„â€¦ procedurÃ„â€¦),
+ -- b) nie sprecyzowano jaki ma byÅ› dokÅ‚adnie format kolumny z kluczem gÅ‚Ã³wnym varchar(7) dopuszcza różne możliwości tzn czy np 11_2020, lis_2020, 11_20
+ -- c) trigger powinien uwzględniać sytuacje, np. usuniecia transakcji na przełomie miesiąca tzn. anulowanie transakcji z danego miesiąca nastąpi w miesiącu następnym,
+ -- d) przy scenariuszu update problem co bedzie podlegało zmianie i jakim zakresie (tzn. jeśli tylko kwota to w związku z tym że teoretyczne brane są pod uwgaę tylko kwoty z obciążenia i
+ --    wypłaty własnej powinny być ujemne jednak nie można wykluczyć że przez pomyłkę użytkownik zmieni kwotę na dodatnią, co powinno być ptraktowano jako wpływ i posiadać inny typ transakcji.    
+ -- e) skad wynika wrtoÅ›c budget_planned czy jest on wpisywany ręcznie za każdym miesiącem czy jest obliczany np. 
+--     jako procent dochodów jeśli obliczana trigger powinien uwzględniać tez kolumne z plan. budżetem.
